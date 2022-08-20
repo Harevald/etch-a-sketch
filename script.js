fed11099f19f21ac0b2d6f13949c8590d5e092ee
Harevald <jakubkrasuski3@gmail.com>
2022-08-20 21:15:01 +0200

    Added styling for whole page, added buttons (no functional yet)

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let grid = document.getElementById('grid');
 let gridItem;
 let gridSize = 16;
-for(let i=0; i<gridSize; i++){
+
+grid.style.gridTemplateColumns = `repeat(${gridSize},1fr)`; //scalable grid
+
+for(let i=0; i<gridSize; i++){ //create gridSize * gridSize amount of divs with gridItem class, and add them to the grid 
     for(let j=0; j<gridSize; j++){
         gridItem = document.createElement('div');
         gridItem.classList.add('gridItem');
